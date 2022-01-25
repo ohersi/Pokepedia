f8cbf7bdd70a143208fbb02f8a3ddb5034c0851f
Omar Hersi <o.hersi@yahoo.com>
2022-01-25 14:18:56 -0500

    created Login component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import React, { useContext } from 'react';
+import { Routes, Route } from 'react-router-dom'
 import './App.css'
 //Components
 import Nav from './components/Nav';
 //Contexts
 import UserContext from './contexts/UserContext';
+//Pages
+import Login from './pages/Login';
 
 const App = () => {
   // In order for us to use context, we import first, then we can use the useContext hook to access our context
@@ -15,6 +17,10 @@ const App = () => {
       {/* All context comes with the Provider Component. This allows us to use this as a wrapper and share information to all of its children. We need the value prop inside our provider.*/}
       <UserContext.Provider value={'Ash Ketchum'}>
         <Nav />
+        {/* We need to wrap our all of our roues inside react outer 'Routes' component */}
+        <Routes>
+          <Route path='login'element={<Login />}/>
+        </Routes>
       </UserContext.Provider>
     </div>
   );
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,22 +1,23 @@
 import React, { useContext } from 'react';
 import UserContext from '../../contexts/UserContext';
-
+import { Link } from 'react-router-dom'
 const Nav = () => {
 
     const user = useContext(UserContext)
     console.log(user)
+    // Below we will use Link from react router to replace all of our anchor tages, we replace the href from <a> to 'to'
     return (
         <div>
             <nav className="navbar navbar-expand-lg navbar-light bg-light">
                 <div className="container-fluid">
-                    <a className="navbar-brand" href="#">Pokepedia</a>
+                    <Link className="navbar-brand" to="/">Pokepedia</Link>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
                         <div className="navbar-nav">
-                            <a className="nav-link active" aria-current="page" href="#">Pokedex</a>
-                            <a className="nav-link" href="#">Login</a>
+                            <Link className="nav-link active" aria-current="page" to="pokedex">Pokedex</Link>
+                            <Link className="nav-link" to="login">Login</Link>
                         </div>
                     </div>
                 </div>
